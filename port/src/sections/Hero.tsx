21a42c4b3526b14504a0f4a20a4f73dba4e15490
mmupfumir <mmupfumir@hartford.edu>
2024-08-29 05:15:25 -0400

    Modified Description

--- port/src/sections/Hero.tsx
+++ port/src/sections/Hero.tsx
@@ -2,13 +2,13 @@ export const Hero = () => {
   return (
     <section id="Hero">
       <div className="container">
-        <div className="flex sm:flex-col md:flex-row md:items-end">
-          <div className="text-black text-7xl tracking-tighter">
+        <div className="flex sm:flex-col md:flex-row md:items-end pt-32 ">
+          <div className="text-black text-7xl tracking-tighter font-medium">
             <h1>Hi,</h1>
-            <h1>I'm Milton.</h1>
+            <h1>I'm <span className="text-black/25">Milton.</span></h1>
           </div>
-          <div className=" text-black/45 sm:pt-10 max-w-72 md:ml-auto">
-            <p>Designer & Developer currently studying <span className="text-black">Architecture</span> & <span className="text-black">CS</span> at the University of Hartford.</p>
+          <div className=" text-black/50 sm:pt-10 max-w-72 md:ml-auto">
+            <p>Designer & Developer currently studying <span className="text-black font-medium italic">Architecture</span> & <span className="text-black font-medium italic">CS</span> at the University of Hartford. I craft <span className="text-black/25 font-medium">modest</span> solutions to the unbuilt environment.</p>
           </div>
         </div>
       </div>
