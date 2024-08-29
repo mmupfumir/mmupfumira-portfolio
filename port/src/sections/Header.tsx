747265a4ccc6bf3573dda8af95f85f2d29afcbc9
mmupfumir <mmupfumir@hartford.edu>
2024-08-29 12:20:45 -0400

    Fixed header indentation

--- port/src/sections/Header.tsx
+++ port/src/sections/Header.tsx
@@ -11,8 +11,25 @@ export const Header = () => {
           alt="Memoji"
           width={50} // Adjust the width and height as needed
           height={50} // Adjust the width and height as needed
-          className="rounded-full object-cover shadow-[0px_0px_20px_#E8E8E8] ml-auto"
+          className="rounded-full object-cover shadow-[0px_0px_20px_#E8E8E8]"
         />
+        <nav className="flex justify-end text-right gap-5">
+          <a href="https://www.linkedin.com/in/milton-mupfumira/" target="_blank" rel="noopener noreferrer" className="text-sm">
+            <div className="text-black">
+              About
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/milton-mupfumira/" target="_blank" rel="noopener noreferrer" className="text-sm">
+            <div className="text-black">
+              Ideas
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/milton-mupfumira/" target="_blank" rel="noopener noreferrer" className="text-sm">
+            <div className="text-black">
+              Read CV
+            </div>
+          </a>
+        </nav>
       </div>
 
     </header>
