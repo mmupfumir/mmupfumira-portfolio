1f13b0b6ea02a6f4f7ca0a1aacabeeb94aa241fc
mmupfumir <mmupfumir@hartford.edu>
2024-08-29 03:31:54 -0400

    Memoji
    Added Memoji (Headshot)

--- port/src/sections/Header.tsx
+++ port/src/sections/Header.tsx
@@ -1,7 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import headshot from "@/assets/headshot.png";
+
 export const Header = () => {
   return (
-    <header className="py-16 border-b border-black/5">
-
+    <header className="py-10">
+      <div className="container flex items-center">
+        <Image
+          src={headshot}
+          alt="Memoji"
+          width={60} // Adjust the width and height as needed
+          height={60} // Adjust the width and height as needed
+          className="rounded-full object-cover shadow-[0px_0px_20px_#E8E8E8]"
+        />
+      </div>
     </header>
   );
 };
